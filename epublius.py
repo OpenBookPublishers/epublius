f317dd198a7a1027f3d3c89434e82dd2ac50dd73
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2016-06-25 06:02:05 +0100

    added files;

---
+++ epublius.py
@@ -0,0 +1,339 @@
+#!/usr/bin/python
+#
+# ePublius
+# Given the contents of an ePub, and indications of specific files of interest
+# (e.g. the filenames of the cover and contents files), and certain templates,
+# it generates a web-friendly version of the ePub's contents.
+#
+# (c) Nik Sultana, Open Book Publishers, September 2013
+# Use of this software is governed by the terms of the GPLv3 -- see LICENSE.
+#
+# Example usage: ./epublius.py -p prefix -s suffix -f 02_title.html -t 06_toc.html -b http://www.openbookpublishers.com/product/97 -d ../epub/OEBPS/ -o ../epub/new/ -h header_add
+#
+# What it does:
+# 1) Unzip epub into directory
+# 2) Shrink images
+# 3) Scrub extra data
+#      remove <link rel="stylesheet" type="application/vnd.adobe-page-template+xml" href="page-template.xpgt"/>
+#      TODO remove font?
+#      TODO rename files
+#
+# TODO:
+# - constrain width
+# - add arrows for forward&backward chapter turns
+#
+# NOTE: it relies on various hacky regexes.
+
+import sys
+import getopt
+import re
+import glob
+import os
+import commands
+#import subprocess
+import string
+import urllib
+
+opts, args = getopt.getopt(sys.argv[1:], "p:s:b:t:f:d:o:h:n:c:r:i:u:k:a:", [])
+
+def usage():
+  print "-p: file containing prefix"
+  print "-s: file containing suffix"
+  print "-b: URL of book's page"
+  print "-t: TOC file"
+  print "-f: Frontpage file"
+  print "-d: Directory prefix (of content, not prefix/suffix)"
+  print "-o: Target directory"
+  print "-h: file containing HTML to inject into header"
+  print "-n: Title name"
+  print "-c: A colophon file (e.g., the license). There may be multiple -c parameters."
+  print "-r: How much to resize the images (as percentage; default is 50)."
+  print "-i: Index file to use."
+  print "-u: URL path of this book"
+  print "-k: Copyright file"
+  print "-a: Donation link"
+
+prefix_file = None
+suffix_file = None
+book_page = None
+toc_file = None
+front_file = None
+directory_prefix = ""
+target_directory = None
+headeradd_file = None
+book_title = ""
+colophon_files = []
+resize_percent = 50
+index_file = ""
+url_prefix = None
+copyright_file = None
+donation_link = None
+
+for o, a in opts:
+  if o == "-p":
+    prefix_file = a
+  elif o == "-s":
+    suffix_file = a
+  elif o == "-b":
+    book_page = a
+  elif o == "-t":
+    toc_file = a
+  elif o == "-f":
+    front_file = a
+  elif o == "-d":
+    directory_prefix = a
+  elif o == "-o":
+    target_directory = a
+  elif o == "-h":
+    headeradd_file = a
+  elif o == "-n":
+    book_title = a
+  elif o == "-c":
+    colophon_files.append(a)
+  elif o == "-r":
+    resize_percent = int(a)
+  elif o == "-i":
+    index_file = a
+  elif o == "-u":
+    url_prefix = a
+  elif o == "-k":
+    copyright_file = a
+  elif o == "-a":
+    donation_link = a
+  else:
+    usage()
+    raise Exception('Unknown argument: ' + o + ':' + a)
+
+if (prefix_file == None or suffix_file == None or book_page == None or
+    toc_file == None or front_file == None):
+  usage()
+  raise Exception('Incomplete parameter list')
+
+if target_directory == None:
+  write_mode = False
+else:
+  write_mode = True
+
+prefix_fd = open(prefix_file)
+prefix = "\n".join(prefix_fd.readlines())
+prefix_fd.close()
+
+suffix_fd = open(suffix_file)
+suffix = "\n".join(suffix_fd.readlines())
+suffix_fd.close()
+
+headeradd_fd = open(headeradd_file)
+header_add = headeradd_fd.readlines()
+headeradd_fd.close()
+
+#include both HTML and CSS files
+matcher_link = re.compile("^.* href=\"([a-zA-Z0-9_.-]+?\.(html|css|xhtml))(#.+)?\".*$")
+
+title_matcher = re.compile("^.*<title>(.+)</title>.*$")
+
+#filter out the "part" pages
+ignore_link = re.compile("^(.*)<a.+href=\"\d+_part\d+\.x?html\".*?>(.+)</a>(.*)$")
+
+body_start = re.compile("^.*<body ?.*>.*$") #FIXME dangerous
+body_end = re.compile("^.*</body>.*$")
+header_end = re.compile("^.*</head>.*$")
+index_link = re.compile("^.*<a.+href=\"((\d+_)?[Ii]ndex\.x?html)\".*?>(INDEX|Index)</a>.*$")
+
+files_done = 0
+
+#maps a page of the book to its previous and next pages. we wrap around: so the
+#last page maps to the contents next, and the contents maps to the last page
+#previous.
+pagecycle = dict()
+
+#populates pagecycle, based on info in toc.ncx
+def extract_pagecycle(path):
+  global pagecycle
+  # from http://stackoverflow.com/questions/19243020/in-python-get-the-output-of-system-command-as-a-string?lq=1
+  #cmd = "grep 'content src' OEBPS/toc.ncx | perl -pe 's/^.+<text>(.+?)<\/text>.+content src="(.+?)(#.*?)?".+$/$1\t$2/'"
+  #cmd = ("/usr/bin/grep 'content src' " + path + "toc.ncx | " +
+  #       "/usr/bin/perl -pe 's/^.+<text>(.+?)<\/text>.+content src=\"(.+?)(#.*?)?\".+$/$2/' | " +
+  #       "/usr/bin/uniq")
+
+  #print ("cmd = " + cmd)
+
+  #proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+  #files_str = proc.stdout.read()
+
+  #factor out unique html files into a list
+  #files = files_str.split()
+
+  files = string.split(commands.getoutput("grep 'content src' " + path + "toc.ncx " +
+                                           "| perl -pe 's/^.+<text>(.+?)<\/text>.+content src=\"(.+?)(#.*?)?\".+$/$2/' " +
+                                           "| uniq"), '\n')
+
+  print ("extracted files = " + str(files))
+
+  #store as tuples in pagecycle: page |-> (prev, next)
+  for i in range(len(files)):
+    print "doing " + str(i) + ": " + files[i]
+    page = files[i]
+    prev = files[i - 1]
+    next = files[(i + 1) % len(files)]
+    pagecycle[page] = (prev, next)
+
+  print ("pagecycle = " + str(pagecycle))
+
+def process_file(filename):
+  global book_title
+  global index_file
+
+  page_suffix = suffix
+  # These re.subs are page-specific.
+  #if filename <> "content.opf":
+  if filename in pagecycle.keys():
+    page_suffix = re.sub("%PREV%", pagecycle[filename][0], page_suffix)
+    page_suffix = re.sub("%NEXT%", pagecycle[filename][1], page_suffix)
+  else:
+    print "No PREV/NEXT for " + filename
+
+  page_prefix = prefix
+  # used for searching within the book
+  page_prefix = re.sub("%THIS_PAGE_URL_PREFIX%", url_prefix + "/*", page_prefix)
+  # used for translating this page
+  page_prefix = re.sub("%THIS_PAGE_URL%", url_prefix + "/" + filename, page_prefix)
+  page_prefix = re.sub("%THIS_PAGE_URL_ENCODED%", urllib.quote(url_prefix + "/" + filename, safe=''), page_prefix)
+
+  links_to = set([])
+  fd = open(directory_prefix + filename)
+  new_contents = []
+  for line in fd.readlines():
+    if line <> '<link rel="stylesheet" type="application/vnd.adobe-page-template+xml" href="page-template.xpgt"/>':
+      match = ignore_link.match(line)
+      if match <> None:
+        if write_mode:
+          new_contents.append(match.group(1) + match.group(2) + match.group(3))
+      else:
+        match = matcher_link.match(line)
+        if match <> None:
+          links_to.add(match.group(1))
+
+        if write_mode:
+          bodystart_match = body_start.match(line)
+          bodyend_match = body_end.match(line)
+          headerend_match = header_end.match(line)
+          title_match = title_matcher.match(line)
+          index_match = index_link.match(line)
+          if bodystart_match <> None:
+            new_contents.append(line)
+            # new_contents.append('<body onload="loader();">\n')
+
+            # new_contents = new_contents + page_prefix
+            new_contents.append(page_prefix)
+          elif bodyend_match <> None:
+            # new_contents = new_contents + page_suffix
+            new_contents.append(page_suffix)
+            new_contents.append(line)
+          elif headerend_match <> None:
+            new_contents = new_contents + header_add
+            new_contents.append(line)
+          elif title_match <> None:
+            new_contents.append(line)
+            if book_title == "":
+              book_title = title_match.group(1)
+              print "Detected book title:" + book_title
+          elif index_match <> None:
+            new_contents.append(line)
+            if index_file == "":
+              index_file = index_match.group(1)
+              print "Detected index file:" + index_file
+          else:
+            new_contents.append(line)
+
+  fd.close()
+
+  if write_mode:
+    fd = open(target_directory + filename, 'w')
+    fd.writelines(new_contents)
+    fd.close()
+
+  return links_to
+
+# List of HTML files
+processed_pages = set([])
+pages_to_process = set([toc_file, os.path.dirname(toc_file) + 'content.opf'])
+
+colophon_links = []
+for colo_file in colophon_files:
+  match = re.search('^(.+_)?(.+)\.x?html?$', colo_file)
+  if match and match.group(2) <> None:
+    #FIXME hardcoded formatting
+    colophon_links.append('<a href="' + colo_file + '">' + match.group(2).title() + '</a>')
+  else:
+    raise Exception('The colophon file ' + colo_file + ' does not match the expected pattern.')
+
+extract_pagecycle(directory_prefix)
+
+while len(pages_to_process) > 0:
+  filename = pages_to_process.pop()
+  print "processing " + filename
+  new_links = process_file(filename)
+  if (files_done == 0): #the first file is processed twice. first time is to extract values, and the second time is to use them.
+    pages_to_process.add(filename)
+
+    #Process files which aren't linked-to by the TOC, such as the Cover and the Colophon.
+    pages_to_process.add(front_file)
+    for colo_file in colophon_files:
+      pages_to_process.add(colo_file)
+    prefix = re.sub("%BOOKTITLE%", book_title, prefix)
+    prefix = re.sub("%TOC%", toc_file, prefix)
+    prefix = re.sub("%BOOKPAGE%", book_page, prefix)
+    prefix = re.sub("%INDEX%", index_file, prefix)
+    prefix = re.sub("%FRONTPAGE%", front_file, prefix)
+    prefix = re.sub("%COLOPHON_LINKS%", 'Colophon: ' + ', '.join(colophon_links), prefix)
+
+    prefix = re.sub("%COPYRIGHT%", copyright_file, prefix)
+    if donation_link != None: prefix = re.sub("%DONATE%", donation_link, prefix)
+
+  else:
+    processed_pages.add(filename)
+    pages_to_process = pages_to_process.union(new_links)
+    pages_to_process.difference_update(processed_pages)
+
+  files_done += 1
+
+print "processed " + str(files_done) + " files"
+#print "processed_pages=" + str(processed_pages)
+#print "pages_to_process=" + str(pages_to_process)
+
+def process_images(directory_prefix, target_directory, path):
+  image_filenames = (map(os.path.basename, glob.glob(directory_prefix + path + '*.jpg')) +
+                     map(os.path.basename, glob.glob(directory_prefix + path + '*.png')))
+  if target_directory <> None:
+    try: os.makedirs(target_directory + path)
+    except: pass
+    for filename in image_filenames:
+      result = commands.getstatusoutput("convert -resize " + str(resize_percent) + "% -quality 80 " +
+                                        "'" + directory_prefix + path + filename + "'" +
+                                        " '" + target_directory + path + filename + "'")
+      print "converting " + filename + ":" + str(result)
+
+print
+print "Now converting images. Resize is " + str(resize_percent) + "%"
+
+#This follows the style of get_file in epublius_wrapper.py
+def get_directory (directory_kind, directory_prefix, directory_heuristics):
+  directory = None
+  for directory_heuristic in directory_heuristics:
+    if os.path.exists(directory_prefix + directory_heuristic):
+      directory = directory_heuristic
+      print "Detected " + directory_kind + " directory: " + directory
+      break
+
+  if directory == None:
+    raise Exception('Could not find ' + directory_kind + ' directory')
+
+  return directory
+
+images_directory = get_directory('Images', directory_prefix, ['images', 'Images', 'image'])
+process_images(directory_prefix, target_directory, images_directory + "/")
+
+css_directory = get_directory('CSS', directory_prefix, ['css'])
+result = commands.getstatusoutput("cp -r " + directory_prefix + "/" + css_directory +
+                                  " " + target_directory + "/" + css_directory)
+print "copying CSS directory (" + css_directory + "):" + str(result)
---
+++ epublius_wrapper.py
@@ -0,0 +1,255 @@
+#!/usr/bin/python
+#
+# ePublius wrapper
+# Discovers the values of parameters to be given to epublius.py.
+# Currently works for -f, -t and -l.
+# Uses toc.ncx to get these values. I also rely on matching
+# filenames, but these often vary wildly. FIXME change the process to rely solely on toc.ncx
+#
+# (c) Nik Sultana, Open Book Publishers, September 2013
+# Use of this software is governed by the terms of the GPLv3 -- see LICENSE.
+#
+# Example usage: ./epublius_wrapper.py -p prefix -s suffix -h header_add -b http://www.openbookpublishers.com/product/97 -f /home/nik/OBP/code/epub/9781906924737_Oral_Literature_in_Africa.epub -o test
+# or: ./epublius_wrapper.py -p prefix -s suffix -h header_add -b http://www.openbookpublishers.com/product/98/women-in-nineteenth-century-russia--lives-and-culture -f testfiles/Women_in_Russia.epub -o WIR2 -n "Women in Russia"
+# or, with variable image resizing: ./epublius_wrapper.py -p prefix -s suffix -h header_add -b http://www.openbookpublishers.com/product/215/ -f 9781783740031_Tacitus_Annals.epub -o TA -n "Tacitus, Annals, 15.20-23, 33-45. Latin Text, Study Aids with Vocabulary, and Commentary" -r 80
+#
+# What it does:
+# - create temp directory
+# - unzip epub into it
+# - detect values of parameters to be passed to epublius.py.
+# - run epublius
+# - remove temp directory
+
+import random
+import time
+import os
+import sys
+import getopt
+import glob
+import commands
+import shutil
+import re
+import string
+
+TMPDIR = "/tmp" #FIXME hardcoded path
+random.seed(str(time.gmtime()))
+
+opts, args = getopt.getopt(sys.argv[1:], "p:s:h:b:f:o:n:z:cr:e:i:u:a:t:", [])
+
+def usage():
+  print "-p: file containing prefix"
+  print "-s: file containing suffix"
+  print "-h: file containing HTML to inject into header"
+  print "-b: URL of book's page"
+  print "-f: ePub file (or contents, if -z is used)"
+  print "-o: Target directory"
+  print "-n: Book name"
+  print "-z: Target of -f parameter is an unzipped ePub file"
+  print "-c: Book doesn't have a cover"
+  print "-r: How much to resize the images (as percentage; default is 50)."
+  print "-e: Location of the ePublius script."
+  print "-i: Force the use of a particular index file. This parameter will simply be passed on to the epublius script."
+  print "-u: URL path of this book"
+  print "-a: Donation link"
+  print "-t: Directory containing the template files (CSS and JS)"
+
+prefix_file = None
+suffix_file = None
+headeradd_file = None
+book_page = None
+epub_file = None
+target_directory = None
+book_title = None
+unzipped_ePub = False
+no_cover = False
+resize_percent = None
+ePublius_path = "."
+index_to_use = None
+url_prefix = None
+donation_link = None
+template_dir = "."
+
+for o, a in opts:
+  if o == "-p":
+    prefix_file = a
+  elif o == "-s":
+    suffix_file = a
+  elif o == "-h":
+    headeradd_file = a
+  elif o == "-b":
+    book_page = a
+  elif o == "-f":
+    epub_file = a
+  elif o == "-o":
+    target_directory = a
+  elif o == "-n":
+    book_title = a
+  elif o == "-z":
+    unzipped_ePub = True
+  elif o == "-c":
+    no_cover = True
+  elif o == "-r":
+    resize_percent = int(a)
+  elif o == "-e":
+    ePublius_path = a
+  elif o == "-i":
+    index_to_use = a
+  elif o == "-u":
+    url_prefix = a
+  elif o == "-a":
+    donation_link = a
+  elif o == "-t":
+    template_dir = a
+  else:
+    usage()
+    raise Exception('Unknown argument: ' + o + ':' + a)
+
+if (prefix_file == None or suffix_file == None or headeradd_file == None or
+    book_page == None or epub_file == None or target_directory == None or
+    url_prefix == None):
+  usage()
+  raise Exception('Incomplete parameter list')
+
+#Transform index_to_use into a parameter for the epublius script.
+if index_to_use == None:
+  index_to_use = "" #No parameter
+else:
+  print "Will use index: " + index_to_use
+  index_to_use = " -i " + index_to_use + " "
+
+def create_tmpdir():
+  tmpdir = None
+  while (tmpdir == None):
+    tmpdir_try = TMPDIR + "/epublius_" + str(random.getrandbits(24)) + "_tmp/"
+    if os.path.exists(tmpdir_try) == False:
+      os.makedirs(tmpdir_try)
+      tmpdir = tmpdir_try
+  return tmpdir
+
+print "epub_file = " + epub_file
+
+if unzipped_ePub:
+  tmpdir = epub_file
+else:
+  tmpdir = create_tmpdir()
+  _, output = commands.getstatusoutput("unzip " + epub_file + " -d " + tmpdir)
+  print output
+
+_, path = commands.getstatusoutput("find " + tmpdir + " -name toc.ncx")
+path = (string.split(path, '\n'))[0] #In case there were multiple finds, we use the first
+print "Temporary directory: " + tmpdir
+path = os.path.dirname(path) + "/"
+print "Expecting to find cover file at: " + path
+
+def get_file (file_kind, file_heuristics):
+  file = None
+  for file_heuristic in file_heuristics:
+    file_attempt = glob.glob(path + file_heuristic)
+    #print "Attempting " + path + file_heuristic + " : " + str(file_attempt)
+    if len(file_attempt) == 1:
+      file = os.path.basename(file_attempt[0])
+      print "Detected " + file_kind + " file: " + file
+      break
+    elif len(file_attempt) > 1:
+      #This is suspicious, since there should be only one match.
+      print "Possible problem: detected " + file_kind + " files:" + str(file_attempt) + " using glob " + file_heuristic
+
+  if file == None:
+    if not unzipped_ePub:
+      shutil.rmtree(tmpdir)
+    raise Exception('Could not find ' + file_kind + ' file')
+
+  return file
+
+
+content_files = string.split(commands.getoutput("grep \"<item id=\" " + path + "content.opf " +
+                                                "| grep 'media-type=\"application/xhtml+xml\"'" +
+                                                "| perl -pe 's/^.+href=\"(.+?)\".+$/$1/'"),
+                             '\n')
+colophon_files = []
+toc_file = None
+title_file = None
+copyright_file = None
+for content_file in content_files:
+  #FIXME could put these heuristics into a config file, or given them at the command line.
+  match_cover = re.search('(_cover.html|cover.html|_Cover.html|Front-cover.xhtml|front-cover.xhtml|00-cover.xhtml)', content_file)
+  if match_cover and title_file == None:
+    title_file = content_file
+
+  #FIXME could put these heuristics into a config file, or given them at the command line.
+  match_toc = re.search('(_toc.html|toc.html|_Contents.html|_Content.html|_Tableofcontent.html|Contents-digital.xhtml|contents.xhtml|Contents.xhtml|Main-text-1.xhtml)', content_file)
+  #NOTE sometimes the files might be contained within a subdirectory -- e.g., 'Text/toc.html' in the case of Yates Annual.
+  #     this is considered as a special case and handled manually, to avoid complicated this code.
+  if match_toc and toc_file == None:
+    toc_file = content_file
+    #Cover should always precede the TOC, so once we find the TOC we can break out of the loop.
+    break
+  else:
+    colophon_files.append(content_file)
+
+  match_copyright = re.search('(copyright.xhtml)', content_file)
+  if match_copyright and copyright_file == None:
+    copyright_file = content_file
+
+if no_cover:
+  title_file = toc_file
+
+print "found toc_file: " + str(toc_file)
+print "found title_file: " + str(title_file)
+print "found copyright_file: " + str(copyright_file)
+
+print "colophon_files=" + str(colophon_files)
+
+#Remove Cover from colophon_files, to avoid it being shown twice in the menu
+# (as an individual link, and as part of the colophon).
+del colophon_files[colophon_files.index(title_file)]
+
+target_directory = target_directory + "/" #FIXME check for this
+if not unzipped_ePub:
+  os.makedirs(target_directory)
+
+# We previously used title_file as the main page, but switched to toc_file in 2016.
+commands.getstatusoutput("cp " + path + "/" + toc_file + " " + path + "/" + "main.html")
+title_file = "main.html"
+
+book_title_arg = ""
+if book_title <> None:
+  book_title_arg = "-n \"" + book_title + "\""
+
+if donation_link == None: donation_link = ""
+else: donation_link = " -a '" + donation_link + "'"
+
+cmd = ("cp " + template_dir + "/html-style.css" +
+       " " + path + "/css/ && " +
+       "cp " + template_dir + "/JS/*" +
+       " " + path + "/")
+print "Copying JS and CSS via command: " + cmd
+_, output = commands.getstatusoutput(cmd)
+
+cmd = (ePublius_path + "/epublius.py -p " + prefix_file + " -s " + suffix_file +
+       " -h " + headeradd_file + " -b " + book_page + " -f " + title_file +
+       " -k " + copyright_file +
+       index_to_use +
+       " -u " + url_prefix +
+       donation_link +
+       " -t " + toc_file + " -d " + path + " -o " + target_directory  +
+       ''.join(map(lambda s: ' -c ' + s, colophon_files)))
+
+if resize_percent <> None:
+ cmd += ' -r ' + str(resize_percent)
+
+_, output = commands.getstatusoutput(cmd + ' ' + book_title_arg)
+print output
+
+# We recopy the file, since when we earlier copied the file it had not yet been
+# processed by epublius, but now it has.
+commands.getstatusoutput("cp " + target_directory + "/" + toc_file + " " + target_directory + "/" + "main.html")
+
+## Append left and right padding requirement to CSS file.
+#cmd = ("echo 'body { padding : 0px 40px 0px 40px; }' >> " + path + "/css/idGeneratedStyles_0.css")
+#print "Appending CSS info via command: " + cmd
+#_, output = commands.getstatusoutput(cmd)
+##
+
+if not unzipped_ePub:
+  shutil.rmtree(tmpdir)
