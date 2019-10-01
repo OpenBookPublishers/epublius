2bcb83b21007cb4d897de609681ccf368476ddc4
Luca Baffa <lb803@mailbox.org>
2019-10-01 09:51:16 +0100

    fix zoom-selector bug

--- JS/zoom-selector.js
+++ JS/zoom-selector.js
@@ -1,11 +1,22 @@
+function is_allowed_size_str(size) {
+    return ['small', 'medium', 'large'].indexOf(size) >= 0;
+}
+
 $(document).ready(function() {
-	if($.cookie('TEXT_SIZE')) {
-		$('div[id="mainwin"]').addClass($.cookie('TEXT_SIZE'));
-	}
-	$('.zoom-selector').click(function() {
-		var textSize = $(this).attr('class');
-		$('div[id="mainwin"]').removeClass('small medium large').addClass(textSize);
-		$.cookie('TEXT_SIZE',textSize, { path: '/', expires: 10000 });
-		return false;
-	});
+    if (is_allowed_size_str($.cookie('TEXT_SIZE'))) {
+        $('div[id="mainwin"]').addClass($.cookie('TEXT_SIZE'));
+    }
+    $('.zoom-selector').click(function() {
+        // we cannot simply use 'small', 'medium', or 'large' as the ID, so
+        // we use 'zoom-{size}' - now we strip that part to obtain the size.
+        var textSize = $(this).attr('id').replace('zoom-', '');
+
+        if (is_allowed_size_str(textSize)) {
+            $('div[id="mainwin"]')
+                .removeClass('small medium large')
+                .addClass(textSize);
+            $.cookie('TEXT_SIZE', textSize, { path: '/', expires: 10000 });
+            return false;
+        }
+    });
 });
