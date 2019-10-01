41fdc4ed140bcdc24c6dbbc5dd1853fbd58114f7
Luca Baffa <lb803@mailbox.org>
2019-10-01 09:32:32 +0100

    remove bootstrap modal plugin support (redundant)

--- JS/images_modal.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-  $('.Image-vertical, .Image-horizontal').on('click', function() {
-  $('.imagepreview').attr('src', $(this).find('img').attr('src'));
-  $('#imagemodal').modal('show');
-  });
-});
