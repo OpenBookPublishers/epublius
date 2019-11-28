b61094e97e86046c58c8bc9b450b09e514b34d3e
Martin Keegan <martin@no.ucant.org>
2019-11-28 18:42:37 +0000

    short circuit out of loop

--- epublius.py
+++ epublius.py
@@ -78,43 +78,44 @@ def process_file(filename, book_title, prefix, suffix, pagecycle, toc_file, url_
   fd = open(directory_prefix + filename)
   new_contents = []
   for line in fd.readlines():
-    if line <> '<link rel="stylesheet" type="application/vnd.adobe-page-template+xml" href="page-template.xpgt"/>':
-      match = ignore_link.match(line)
+    if line == '<link rel="stylesheet" type="application/vnd.adobe-page-template+xml" href="page-template.xpgt"/>':
+      continue
+    match = ignore_link.match(line)
+    if match <> None:
+      if write_mode:
+        new_contents.append(match.group(1) + match.group(2) + match.group(3))
+    else:
+      match = matcher_link.match(line)
       if match <> None:
-        if write_mode:
-          new_contents.append(match.group(1) + match.group(2) + match.group(3))
-      else:
-        match = matcher_link.match(line)
-        if match <> None:
-          links_to.add(match.group(1))
+        links_to.add(match.group(1))
 
-        if write_mode:
-          bodystart_match = body_start.match(line)
-          bodyend_match = body_end.match(line)
-          headerend_match = header_end.match(line)
-          title_match = title_matcher.match(line)
-          index_match = index_link.match(line)
-          if bodystart_match <> None:
-            new_contents.append(line)
-            new_contents.append(page_prefix)
-          elif bodyend_match <> None:
-            new_contents.append(page_suffix)
-            new_contents.append(line)
-          elif headerend_match <> None:
-            new_contents = new_contents + header_add
-            new_contents.append(line)
-          elif title_match <> None:
-            new_contents.append(line)
-            if book_title == "":
-              book_title = title_match.group(1)
-              print "Detected book title:" + book_title
-          elif index_match <> None:
-            new_contents.append(line)
-            if index_file == "":
-              index_file = index_match.group(1)
-              print "Detected index file:" + index_file
-          else:
-            new_contents.append(line)
+      if write_mode:
+        bodystart_match = body_start.match(line)
+        bodyend_match = body_end.match(line)
+        headerend_match = header_end.match(line)
+        title_match = title_matcher.match(line)
+        index_match = index_link.match(line)
+        if bodystart_match <> None:
+          new_contents.append(line)
+          new_contents.append(page_prefix)
+        elif bodyend_match <> None:
+          new_contents.append(page_suffix)
+          new_contents.append(line)
+        elif headerend_match <> None:
+          new_contents = new_contents + header_add
+          new_contents.append(line)
+        elif title_match <> None:
+          new_contents.append(line)
+          if book_title == "":
+            book_title = title_match.group(1)
+            print "Detected book title:" + book_title
+        elif index_match <> None:
+          new_contents.append(line)
+          if index_file == "":
+            index_file = index_match.group(1)
+            print "Detected index file:" + index_file
+        else:
+          new_contents.append(line)
 
   fd.close()
 
