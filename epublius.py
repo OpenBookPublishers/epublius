2a8a8948224be77637a3986ef2c0ee7cb1920983
Martin Keegan <martin@no.ucant.org>
2019-11-29 14:26:33 +0000

    join paths properly

--- epublius.py
+++ epublius.py
@@ -295,10 +295,13 @@ def process_images_and_css(directory_prefix, resize_percent, target_directory):
                  resize_percent)
 
   css_directory = get_directory('CSS', directory_prefix, ['css'])
-  result = commands.getstatusoutput("cp -r " + directory_prefix + "/" +
-                                    css_directory +
-                                    " " + target_directory + "/" +
-                                    css_directory)
+  
+  result = commands.getstatusoutput(
+    "cp -r " + 
+    os.path.join(directory_prefix, css_directory) +
+    " " +
+    os.path.join(target_directory, css_directory)
+  )
 
   print "copying CSS directory (" + css_directory + "):" + str(result)
 
