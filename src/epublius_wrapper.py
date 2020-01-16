7effe010b14e7a641010506b0e03abfb34c853c9
Luca Baffa <luca@openbookpublishers.com>
2020-01-16 17:30:40 +0000

    fix zip archive
    Compress output directory maintaining folder hierarchy.

--- src/epublius_wrapper.py
+++ src/epublius_wrapper.py
@@ -368,13 +368,22 @@ def process_epub(args):
   if not unzipped_ePub:
     shutil.rmtree(tmpdir)
 
-  out_file = os.path.normpath(target_directory) + '.zip'
-  with ZipFile(out_file, 'w') as zipfile:
-    for root, dirs, files in os.walk(target_directory):
-      for file in files:
-        filename = os.path.join(os.path.basename(os.path.normpath(
-            target_directory)), file)
-        zipfile.write(os.path.join(root, file), filename)
+  # ZIP OUTPUT FOLDER
+  # if target_directory looks like:
+  # ../htmlreader_output/978-1-78374-863-1/
+
+  # -> base_dir = '978-1-78374-863-1'
+  base_dir = target_directory.split('/')[-2]
+
+  # -> root_dir = '../htmlreader_output'
+  root_dir = '/'.join([target_directory.split('/')[0],
+                       target_directory.split('/')[1]])
+
+  shutil.make_archive(base_dir=base_dir,
+                      root_dir=root_dir,
+                      format='zip',
+                      base_name=target_directory)
+
   shutil.rmtree(target_directory)
 
 if __name__ == '__main__':
