ebb287ac6a7c354277638645db3c09d7a57b78d6
Luca Baffa <luca@openbookpublishers.com>
2020-02-05 15:38:44 +0000

    get rid of some old code

--- src/main.py
+++ src/main.py
@@ -69,16 +69,5 @@ def main():
         
 
 
-    # ZIP OUTPUT FOLDER
-    # if target_directory looks like:
-    # ../htmlreader_output/978-1-78374-863-1/
-
-    # -> base_dir = '978-1-78374-863-1'
-    base_dir = target_directory.split('/')[-2]
-
-    # -> root_dir = '../htmlreader_output'
-    root_dir = '/'.join([target_directory.split('/')[0],
-                         target_directory.split('/')[1]])
-
 if __name__ == '__main__':
     main()
