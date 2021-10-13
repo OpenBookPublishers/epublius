e6a434b5c36d858aa504367ec2ed5a6f1a3185c9
Luca Baffa <luca@openbookpublishers.com>
2021-10-13 16:08:13 +0100

    show a notification

--- src/includes/js/cookie-popup.js
+++ src/includes/js/cookie-popup.js
@@ -1,7 +1,10 @@
 jQuery(document).ready(function() {
 
     if(localStorage.getItem('cookies') != 'notified'){
-	window.alert('cookies alert');
+	// Normally we would use UIkit.notification('message');
+	// but for XHTML compatibility I am toggling visibility
+	// of an element on the page.
+	$("#cookies-consent").removeClass("uk-hidden");
 
 	localStorage.setItem('cookies', 'notified');
     }
