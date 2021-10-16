f43b3b36b72efc4b14ef24c483b61c3c92f89ac5
Luca Baffa <luca@openbookpublishers.com>
2021-10-16 08:36:36 +0100

    replace indentation tabs with spaces

--- src/includes/js/zoom-selector.js
+++ src/includes/js/zoom-selector.js
@@ -8,23 +8,23 @@ document.addEventListener("DOMContentLoaded", function() {
     let mainwin = document.querySelector('#mainwin')
 
     if (is_allowed_size_str(textSize)) {
-	mainwin.classList.add(textSize);
+        mainwin.classList.add(textSize);
     }
 
     const selectors = document.querySelectorAll('.zoom-selector');
 
     selectors.forEach(el => el.addEventListener('click', event => {
-	// get the text size information form the button id ('zoom-{size}')
-	const button = event.target.parentNode;
+        // get the text size information form the button id ('zoom-{size}')
+        const button = event.target.parentNode;
         textSize = button.getAttribute('id').replace('zoom-', '');
 
         if (is_allowed_size_str(textSize)) {
             mainwin.classList.remove('small', 'medium', 'large')
             mainwin.classList.add(textSize);
 
-	    localStorage.setItem('text-size', textSize);
+            localStorage.setItem('text-size', textSize);
         }
-	// prevent the <a href... element from firing when clicked
-	return false;
+        // prevent the <a href... element from firing when clicked
+        return false;
     }));
 });
