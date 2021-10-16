bf71df5e9a2ca75c0856a44a0e5b4fca25dab964
Luca Baffa <luca@openbookpublishers.com>
2021-10-16 08:47:53 +0100

    refine var scope plus fix typos

--- src/includes/js/zoom-selector.js
+++ src/includes/js/zoom-selector.js
@@ -4,8 +4,8 @@ function is_allowed_size_str(size) {
 
 document.addEventListener("DOMContentLoaded", function() {
 
-    let textSize = localStorage.getItem('text-size');
-    let mainwin = document.querySelector('#mainwin')
+    var textSize = localStorage.getItem('text-size');
+    var mainwin = document.querySelector('#mainwin');
 
     if (is_allowed_size_str(textSize)) {
         mainwin.classList.add(textSize);
@@ -14,12 +14,12 @@ document.addEventListener("DOMContentLoaded", function() {
     const selectors = document.querySelectorAll('.zoom-selector');
 
     selectors.forEach(el => el.addEventListener('click', event => {
-        // get the text size information form the button id ('zoom-{size}')
+        // get the text size information from the button id ('zoom-{size}')
         const button = event.target.parentNode;
         textSize = button.getAttribute('id').replace('zoom-', '');
 
         if (is_allowed_size_str(textSize)) {
-            mainwin.classList.remove('small', 'medium', 'large')
+            mainwin.classList.remove('small', 'medium', 'large');
             mainwin.classList.add(textSize);
 
             localStorage.setItem('text-size', textSize);
